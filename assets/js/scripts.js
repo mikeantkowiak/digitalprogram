b2e53dc4ba4f3ad6229ff58a9f8fac347a7b2e95
Michael Antkowiak <mike@MacBook-Pro.local>
2019-12-02 23:38:04 -0500

    more js refactoring clean up hero clean up scrollto positios

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,37 +1,36 @@
 
 jQuery(document).ready(function ($) {
 
-    $('.loadingDiv').animate({
-        opacity: '1',
-        rotation: 720
-    },{
-        step: function (now, fx) {
-            $(this).css('-webkit-transform', 'rotateZ(' + now + 'deg)');
-            $(this).css('-moz-transform', 'rotateZ(' + now + 'deg)');
-            $(this).css('transform', 'rotateZ(' + now + 'deg)');
-        }
-    }, 'easeInOutQuint');
+    var offsetScroll = 100;
+    var aArray = getHrefs();
+ 
 
-    jQuery("body").show();
+    $(window).resize(function () {
+        var viewportWidth = $(window).width();
 
-    $(document).ready(function () {
-        var classCycle = ['imageCycle1', 'imageCycle2', 'imageCycle3', 'imageCycle4'];
-        var randomNumber = Math.floor(Math.random() * classCycle.length);
-        var classToAdd = classCycle[randomNumber];
-        $('#hero').addClass(classToAdd);
+        if ( viewportWidth > 768){
+            offsetScroll = 160;
+        } else{
+            offsetScroll = 100;
+        } 
+        console.log(offsetScroll);
     });
 
+    
+     /* Animation Controls */
+    function siteAnimations(){      
+        $('.loadingDiv').animate({
+            opacity: '1',
+            rotation: 720
+        }, {
+            step: function (now, fx) {
+                $(this).css('-webkit-transform', 'rotateZ(' + now + 'deg)');
+                $(this).css('-moz-transform', 'rotateZ(' + now + 'deg)');
+                $(this).css('transform', 'rotateZ(' + now + 'deg)');
+            }
+        }, 'easeInOutQuint');
 
-    $(document).on('closing', '.remodal', function (e) {
-        $("iframe").each(function () {
-            var src = $(this).attr('src');
-            $(this).attr('src', src);
-        });
-
-    });
-
-    $(window).ready(function () {
-        // this can be reworked to use css animate
+        $("body").show();
         $('#loading').delay(1000).fadeOut();
         $('main').delay(2000).fadeIn(2000);
         $('.header').delay(900).animate({
@@ -41,16 +40,30 @@ jQuery(document).ready(function ($) {
         $('.coming-soon-text').delay(2000).animate({
             opacity: "1",
         }, 1500);
-    });
-    /** 
-     * This part does the "fixed navigation after scroll" functionality
-     * We use the jQuery function scroll() to recalculate our variables as the 
-     * page is scrolled/
-     */
-    $(window).scroll(function () {
-        var windowTop = $(window).scrollTop(); // the "12" should equal the margin-top value for nav.stick
-        var navTop = $('#nav-anchor').offset().top;
+    }
 
+
+   
+    /* Nav css and scroll controls */
+    $("nav a, .arrow-down").click(function (evn) {
+        evn.preventDefault();
+        $('html,body').scrollTo(this.hash, this.hash, { offset: -(offsetScroll) });
+    });
+
+    function getHrefs() {
+        var aChildren = $("nav li").children();
+        var aArrayRefs = [];
+        for (var i = 0; i < aChildren.length; i++) {
+            var aChild = aChildren[i];
+            var ahref = $(aChild).attr('href');
+            aArrayRefs.push(ahref);
+        }
+        return aArrayRefs;
+    };
+
+    function scrollClasses() {
+        var windowTop = $(window).scrollTop();
+        var navTop = $('#nav-anchor').offset().top;
         if (windowTop > navTop) {
             $('#content').addClass('extraspace');
             $('nav').addClass('stick');
@@ -58,36 +71,15 @@ jQuery(document).ready(function ($) {
             $('#content').removeClass('extraspace');
             $('nav').removeClass('stick');
         }
-    });
-    var offsetScroll = 160;
-    /**
-     * This part causes smooth scrolling using scrollto.js
-     */
-    $("nav a").click(function (evn) {
-        evn.preventDefault();
-        $('html,body').scrollTo(this.hash, this.hash, { offset: -offsetScroll});
-    });
-    $(".arrow-down").click(function (evn) {
-        evn.preventDefault();
-        $('html,body').scrollTo(this.hash, this.hash, { offset: -offsetScroll });
-    });
-
-    /**
-     * This part handles the highlighting functionality.
-     */
-    var aChildren = $("nav li").children();
-    var aArray = [];
-
-    for (var i = 0; i < aChildren.length; i++) {
-        var aChild = aChildren[i];
-        var ahref = $(aChild).attr('href');
-        aArray.push(ahref);
     }
 
-    $(window).scroll(function () {
-        var windowPos = $(window).scrollTop() + offsetScroll; // get the offset of the window from the top of page
-        var windowHeight = $(window).height(); // get the height of the window
+    function navHighlights(){
+
+        /*I CAN FIGURE THIS OUT!! */
+        var windowPos = $(window).scrollTop() + offsetScroll; 
+        var windowHeight = $(window).height();
         var docHeight = $(document).height();
+        var navTop = $('.main-nav').height();
 
         for (var i = 0; i < aArray.length; i++) {
             var theID = aArray[i];
@@ -95,15 +87,14 @@ jQuery(document).ready(function ($) {
             if (!divPosid.length) {
                 return;
             }
-            var divPos = divPosid.offset().top; // get the offset of the div from the top of page
-            var divHeight = $(theID).height(); // get the height of the div in question
+            var divPos = parseInt(divPosid.offset().top);
+            var divHeight = $(theID).height();
             if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
                 $("a[href='" + theID + "']").addClass("nav-active");
             } else {
                 $("a[href='" + theID + "']").removeClass("nav-active");
             }
         }
-
         if (windowPos + windowHeight === docHeight) {
             if (!$("nav li:last-child a").hasClass("nav-active")) {
                 var navActiveCurrent = $(".nav-active").attr("href");
@@ -111,6 +102,14 @@ jQuery(document).ready(function ($) {
                 $("nav li:last-child a").addClass("nav-active");
             }
         }
+    }
+
+    siteAnimations();
+
+    $(window).scroll(function () {
+        scrollClasses();
+        navHighlights();
     });
+
 });
 
